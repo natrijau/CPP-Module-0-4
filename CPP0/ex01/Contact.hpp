71d4bd52d77af0b6fd2c5cfc0456ecbe34abe5c6
natrijau <>
2024-11-28 11:02:42 +0100

    push all names fils

---
+++ CPP0/ex01/Contact.hpp
@@ -0,0 +1,13 @@
+#ifndef CONTACT_H
+#define CONTACT_H
+
+class Contact
+{
+	private:
+        const char *name;
+        const int   number;
+	public:
+
+};
+
+#endif
---
+++ CPP0/ex01/Phonebook.hpp
@@ -0,0 +1,22 @@
+#ifndef PHONEBOOK_H
+#define PHONEBOOK_H
+
+class phonebook
+{
+private:
+    /* data */
+public:
+    phonebook(/* args */);
+    ~phonebook();
+};
+
+phonebook::phonebook(/* args */)
+{
+}
+
+phonebook::~phonebook()
+{
+}
+
+
+#endif
